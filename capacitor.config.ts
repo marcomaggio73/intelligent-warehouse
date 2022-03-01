ef12c4e26dbf6e57ec9a3de2b358a845ee13d2b3
Marco <67362818+marcomaggio73@users.noreply.github.com>
2022-03-01 13:58:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'intelligent-warehouse',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
